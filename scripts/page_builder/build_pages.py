3382d1be35b1166b3fd0a4cad673b90b90ff1977
Alan Smith <commit@alanwsmith.com>
2022-04-09 22:35:59 -0400

    moving page builder into this project from the generic ascii_art project

---
+++ scripts/page_builder/build_pages.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import PIL.Image
+from PIL import ImageOps
+import os
+
+from pathlib import Path
+from string import Template
+
+
+ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', ' ']
+
+def resize_image(image, new_width):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+
+def make_grayscale(image):
+
+    # grayscaled = image.convert('L')
+    grayscaled = ImageOps.grayscale(image)
+    return grayscaled
+
+
+def make_ascii(image):
+    pixels = image.getdata()
+    characters = ''.join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return characters
+
+
+def main(width, input_path, output_path, letter_padding):
+    image = PIL.Image.open(input_path)
+    new_sized = resize_image(image, width)
+    new_grayscale = make_grayscale(new_sized)
+    raw_ascii = make_ascii(new_grayscale)
+    the_ascii = ''
+    for indx, i in enumerate(raw_ascii):
+        if indx % width == 0:
+            the_ascii += "\n"
+        the_ascii += i
+        for pad in range(0, letter_padding):
+            the_ascii += " "
+    return the_ascii
+
+if __name__ == '__main__':
+
+    pages = [
+        {
+            "id": "safari",
+            "template": 'template_safari.html',
+            "count": 9,
+            "width": 40,
+            "input_dir": "../../_assets/frames/pngs-v2-only-half",
+            "output_dir": "../../site/frames",
+            "letter_padding": 1,
+        }
+    ]
+
+    for page in pages:
+        # print(page['id'])
+        with open(page['template']) as _template:
+            frame = Template(_template.read())
+            for i in range(1, page['count']):
+                next_frame = i + 1
+                if next_frame == page['count']:
+                    next_frame = 1
+                input_path = f"{page['input_dir']}/{i}.png"
+                output_path = f"{page['output_dir']}/{i}.html"
+                output_ascii = main(page['width'], input_path, output_path, page['letter_padding'])
+                with open(output_path, 'w') as _out:
+                    _out.write(frame.substitute(
+                        next_frame = next_frame,
+                        the_ascii = output_ascii
+                    ))
+
+
+
+
+
+
+    # count = 17 
+    # count = 9
+
+    #width = 40
+    # The letter_padding helps deal with the difference in the
+    # lenght and height of letters when using straight html
+
+    # letter_padding = 1
+    # for i in range(1, count):
+    #     next_frame = i + 1
+    #     if next_frame == count:
+    #         next_frame = 1
+    #     input_path = f"/Users/alan/workshop/html-animation.alanwsmith.com/_assets/frames/pngs-v2-only-half/{i}.png"
+    #     output_path = f'/Users/alan/workshop/html-animation.alanwsmith.com/site/frames/{i}.html'
+    #     # input_path = f"/Users/alan/workshop/html-animation.alanwsmith.com/_assets/frames/pngs-v2/{i}.png"
+    #     # output_path = f'/Users/alan/workshop/html-animation.alanwsmith.com/site/frames-chrome/{i}.html'
+    #     output_ascii = main(width, input_path, output_path, letter_padding)
+    #     with open(output_path, 'w') as _out:
+    #         _out.write(frame.substitute(
+    #             next_frame = next_frame,
+    #             the_ascii = output_ascii
+    #         ))
+
+
